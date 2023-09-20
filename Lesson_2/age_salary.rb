9d45622d2d7f1675f369a7ab3de629a97165342b
Rinat273 <bairam0487@gmail.com>
2023-09-20 18:04:36 +0400

    Update age_salary

--- Lesson_2/age_salary.rb
+++ Lesson_2/age_salary.rb
@@ -1,6 +1,19 @@
 puts "Enter your salary: "
+
 salary = gets.to_i
 
-age_salary = salary * 12
+puts "Enter the number of years to calculate the deposit: "
 
-puts "Your salary for the year is #{age_salary} "
+years = gets.to_i
+
+year_salary = salary * 12
+
+save_money = (salary * 0.15).ceil(2)
+
+save_money_years = (save_money * 12) * years
+
+puts "Your salary for the year is #{year_salary} usd"
+
+puts "You saved for a month #{save_money} usd"
+
+puts "You saved for #{years} years #{save_money_years} usd"
