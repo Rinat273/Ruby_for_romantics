25b54a5619a39106fc356547c0e2e7ceddd79f00
Rinat273 <bairam0487@gmail.com>
2023-09-20 20:40:44 +0400

    Update house_credit.rb

--- Lesson_2/house_credit.rb
+++ Lesson_2/house_credit.rb
@@ -1,13 +1,11 @@
-years = 30
+price = 500_000
+interest = 0.04
+annual_payment = 16_666
 
-pay = 500000
+30.times do |n|
+  remaining = price - n * annual_payment
+  interest_payment = remaining * interest
+  total = annual_payment + interest_payment
 
-Y = pay / years
-
-30.times do |i|
-
-pay = pay - Y
-
-puts "В #{i+1} год осталось заплатить #{pay} usd"
-
+  puts "Year #{n}, left to pay: #{remaining}. You are paying #{annual_payment} plus #{interest_payment} of interest (total is #{total})"
 end
